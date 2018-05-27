2891146405257b858d8a3868b2677c004ecac3d9
gonzalesmk <judcnegri@gmail.com>
2018-05-27 12:41:43 -0300

    Implemented Map Service Node

--- src/positioning_system.cpp
+++ src/positioning_system.cpp
@@ -2,68 +2,34 @@
 #include <std_msgs/Float32.h>
 #include <geometry_msgs/Twist.h>
 #include <tf/transform_listener.h>
-
-// Especifica a orientação do robô
-enum Orientation
-{
-    norte,
-    sul,
-    leste,
-    oeste
-};
-enum Orientation2
-{
-    trem,
-    inicio,
-    azul,
-    verde
-};
-
-// Especifica as posições do robô
-enum Location
-{
-    quadrante_central,
-    quadrante_esquerdo,
-    quadrante_direito,
-    doca_verde,
-    doca_azul,
-    doca_central,
-    trem
-};
-
-struct Pose{
-    Orientation orientation;
-    Location location; 
-}
+#include <projeto_semear/Pose.h>
+#include <projeto_semear/GetPose.h>
 
 /// Need a Global Variable to Keep Track of Position and Orientation
-Orientation g_orient;
-Position g_pose;
 
-// Serv
-bool get_pose(beginner_tutorials::AddTwoInts::Request  &req,
-         beginner_tutorials::AddTwoInts::Response &res)
+projeto_semear::Pose g_pose;
+
+// Services
+bool gps(projeto_semear::GetPose::Request &req,
+         projeto_semear::GetPose::Response &res)
 {
-  res.sum = req.a + req.b;
-  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
-  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
-  return true;
-}
+    req.set ? g_pose = req.pose : res.pose = g_pose;
 
+    return true;
+}
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "moveEletroima");
+    ros::init(argc, argv, "GPS");
     ros::NodeHandle node;
 
     // Initial Values
-    g_orient = norte;
-    g_pose = doca_central;
+    g_pose.orientation = projeto_semear::Pose::ORIENTATION_TREM;
+    g_pose.location = projeto_semear::Pose::DOCA_CENTRAL;
 
     // Cria o serviço
-    ros::ServiceServer service = n.advertiseService("get_pos_and_orient", get_pose);
-    ROS_INFO("Ready to add two ints.");
-    ros::spin();
+    ros::ServiceServer service = node.advertiseService("gps", gps);
 
+    ros::spin();
     return 0;
 }
