b9296b9a732877501afc0f462654443d0d9d16c0
GMistico <giovane.mistico@gmail.com>
2018-06-07 18:53:03 -0300

    Mensagem Colors criada, Service parameters EscolherContainer criado. Service escolher_container criado

---
+++ src/escolher_container.cpp
@@ -0,0 +1,28 @@
+/** Esse programa é um SERVIÇO responsável por escolher qual container deve pegar
+ * 
+ * Definição do serviço EscolherContainer: 
+ *         cor do da esquerda
+ *         cor do da direita
+ *         ---
+ *         retorno
+ * 
+ *  As entradas são as cores do container da esquerda e da direita logo à frente do container
+ *  A saída é o container escolhido
+ * 
+ * Nome do serviço : EscolherContainer
+ * */
+
+#include "ros/ros.h"
+#include "projeto_semear18/EscolherContainer.h"
+
+bool add(projeto_semear18::EscolherContainer::Request &req,
+         projeto_semear18::EscolherContainer::Response &res)
+    {
+        
+    }
+
+int main()
+    {
+        
+    }
+
