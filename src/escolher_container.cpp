0bcd20095d25f657a4f3f62de744087fe65d8ee3
GMistico <giovane.mistico@gmail.com>
2018-06-13 11:49:27 -0300

    servico melhorado, nao pronto ainda

--- src/escolher_container.cpp
+++ src/escolher_container.cpp
@@ -12,17 +12,42 @@
  * Nome do serviço : EscolherContainer
  * */
 
+//esse código considera que o navio verde estará NECESSARIAMENTE à esquerda
 #include "ros/ros.h"
-#include "projeto_semear18/EscolherContainer.h"
+#include "projeto_semear/EscolherContainer.h"
+#include "projeto_semear/Colors.h"
 
-bool add(projeto_semear18::EscolherContainer::Request &req,
+
+bool escolha(projeto_semear18::EscolherContainer::Request &req,
          projeto_semear18::EscolherContainer::Response &res)
     {
+        if(req.Cor_Esquerda == 1){return 0}
+        else if(req.Cor_Esquerda == 2){return 1}
+        else if(req.Cor_Esquerda == 2 && req.Cor_Direita == 1){return 0}
+        else if(req.Cor_Esquerda == 2 && req.Cor_Direita == 0){return 0}
+        else if(req.Cor_Esquerda == 0 && req.Cor_Direita == 2){return 1}
+        else if(req.Cor_Esquerda == 0 && req.Cor_Direita == 0){return 2}
         
     }
 
-int main()
-    {
-        
-    }
+/**
+ * 
+ * int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "escolher_container");
+    ros::NodeHandle node;
+
+    colors_client = node.serviceClient<projeto_semear::Colors>("EscolherContainer"); // Requisita o serviço EscolherContainer
+
+    // Cria o action server
+    Server server(node, "navigation", boost::bind(&execute, _1, &server), false);
+    server.start();
+
+    ros::spin();
+
+    return 0;
+}
+
+**/
+
 
