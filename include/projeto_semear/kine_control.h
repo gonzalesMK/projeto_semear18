a9c83cc14bbcf51ca04c627fdba90a4672a077be
MariaLuisaNascimento <maluisa.sert@hotmail.com>
2018-07-24 11:32:03 -0300

    Configuração dos sensores da frente

--- include/projeto_semear/kine_control.h
+++ include/projeto_semear/kine_control.h
@@ -62,10 +62,19 @@ protected:
   ros::NodeHandle nh_;
 
 public:
-  color colorFL_= BRANCO;
+  color colorFL_= BRANCO ; 
   color colorBL_= BRANCO ;
   color colorFR_= BRANCO ;
   color colorBR_= BRANCO ;
+
+  color colorR0_= BRANCO ; 
+  color colorR1_= BRANCO ;
+  color colorR2_= BRANCO ;
+  color colorR3_= BRANCO ;
+  color colorL0_= BRANCO ; 
+  color colorL1_= BRANCO ;
+  color colorL2_= BRANCO ;
+  color colorL3_= BRANCO ;
 
   ros::Publisher FR_Motor_;
   ros::Publisher FL_Motor_;
@@ -76,6 +85,15 @@ public:
   ros::Subscriber lineSensorFR_;
   ros::Subscriber lineSensorBL_;
   ros::Subscriber lineSensorBR_;
+
+    ros::Subscriber ColorSensorR0_;
+    ros::Subscriber ColorSensorR1_;
+    ros::Subscriber ColorSensorR2_;
+    ros::Subscriber ColorSensorR3_;
+    ros::Subscriber ColorSensorL0_;
+    ros::Subscriber ColorSensorL1_;
+    ros::Subscriber ColorSensorL2_;
+    ros::Subscriber ColorSensorL3_;
 
   robot();
 
@@ -97,6 +115,15 @@ public:
   color get_colorBL();
   color get_colorFR();
   color get_colorBR();
+
+  color get_colorR0();
+  color get_colorR1();
+  color get_colorR2();
+  color get_colorR3();
+  color get_colorL0();
+  color get_colorL1();
+  color get_colorL2();
+  color get_colorL3();
 };
 
 // Função para mudar o quadrante do robô - Deve funcionar como seguidor de linha
@@ -111,6 +138,7 @@ void alinhar(kineControl::robot &robot);
 void ir_doca(kineControl::robot &robot);
 void ir_quadrante(kineControl::robot &robot);
 void alinhar_doca(kineControl::robot &robot);
+void alinhar_containerdepositado(kineControl::robot &robot);
 
 
 } // namespace kineControl
--- src/convert_colorsensor.cpp
+++ src/convert_colorsensor.cpp
@@ -22,10 +22,28 @@ ros::Publisher pubFR;
 ros::Publisher pubBL;
 ros::Publisher pubFL;
 
+ros::Publisher pubR0;
+ros::Publisher pubR1;
+ros::Publisher pubR2;
+ros::Publisher pubR3;
+ros::Publisher pubL0;
+ros::Publisher pubL1;
+ros::Publisher pubL2;
+ros::Publisher pubL3;
+
 void callbackBR(const sensor_msgs::ImageConstPtr &msg);
 void callbackBL(const sensor_msgs::ImageConstPtr &msg);
 void callbackFR(const sensor_msgs::ImageConstPtr &msg);
 void callbackFL(const sensor_msgs::ImageConstPtr &msg);
+
+void callbackR0(const sensor_msgs::ImageConstPtr &msg);
+void callbackR1(const sensor_msgs::ImageConstPtr &msg);
+void callbackR2(const sensor_msgs::ImageConstPtr &msg);
+void callbackR3(const sensor_msgs::ImageConstPtr &msg);
+void callbackL0(const sensor_msgs::ImageConstPtr &msg);
+void callbackL1(const sensor_msgs::ImageConstPtr &msg);
+void callbackL2(const sensor_msgs::ImageConstPtr &msg);
+void callbackL3(const sensor_msgs::ImageConstPtr &msg);
 
 int main(int argc, char **argv)
 {
@@ -38,11 +56,28 @@ int main(int argc, char **argv)
     ros::Subscriber subFR = n.subscribe("/AMR/lineSensorFR", 1000, callbackFR);
     ros::Subscriber subFL = n.subscribe("/AMR/lineSensorFL", 1000, callbackFL);
 
+    ros::Subscriber subR0 = n.subscribe("/AMR/ColorSensorR0", 1000, callbackR0);
+    ros::Subscriber subR1 = n.subscribe("/AMR/ColorSensorR1", 1000, callbackR1);
+    ros::Subscriber subR2 = n.subscribe("/AMR/ColorSensorR2", 1000, callbackR2);
+    ros::Subscriber subR3 = n.subscribe("/AMR/ColorSensorR3", 1000, callbackR3);
+    ros::Subscriber subL0 = n.subscribe("/AMR/ColorSensorL0", 1000, callbackL0);
+    ros::Subscriber subL1 = n.subscribe("/AMR/ColorSensorL1", 1000, callbackL1);
+    ros::Subscriber subL2 = n.subscribe("/AMR/ColorSensorL2", 1000, callbackL2);
+    ros::Subscriber subL3 = n.subscribe("/AMR/ColorSensorL3", 1000, callbackL3);
+
     pubBR = n.advertise<std_msgs::Float32>("/image_converter/lineSensorBR", 1);
     pubFR = n.advertise<std_msgs::Float32>("/image_converter/lineSensorFR", 1);
     pubBL = n.advertise<std_msgs::Float32>("/image_converter/lineSensorBL", 1);
     pubFL = n.advertise<std_msgs::Float32>("/image_converter/lineSensorFL", 1);
 
+    pubR0 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorR0", 1);
+    pubR1 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorR1", 1);
+    pubR2 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorR2", 1);
+    pubR3 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorR3", 1);   
+    pubL0 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorL0", 1);
+    pubL1 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorL1", 1);
+    pubL2 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorL2", 1);
+    pubL3 = n.advertise<std_msgs::Float32>("/image_converter/ColorSensorL3", 1);
     ros::spin();
 
     return 0;
@@ -76,3 +111,68 @@ void callbackFL(const sensor_msgs::ImageConstPtr &msg)
 
     pubFL.publish(message);
 }
+
+void callbackL0(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubR0.publish(message);
+}
+
+void callbackL1(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubR1.publish(message);
+}
+
+void callbackL2(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubR2.publish(message);
+}
+
+void callbackL3(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubR3.publish(message);
+}
+
+void callbackR0(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubL0.publish(message);
+}
+
+void callbackR1(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubL1.publish(message);
+}
+
+void callbackR2(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubL2.publish(message);
+}
+
+void callbackR3(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubL3.publish(message);
+}
+
