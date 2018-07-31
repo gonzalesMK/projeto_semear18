c32fe61763f5354e189410e47e372830e33ec6a7
MariaLuisaNascimento <maluisa.sert@hotmail.com>
2018-07-31 00:46:44 -0300

    Cliente teste depositar_container

---
+++ src/depositar_client.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <projeto_semear/DepositarContainer.h>
+#include <actionlib/client/simple_action_client.h>
+#include <projeto_semear/moveEletroimaAction.h>
+#include <std_msgs/Bool.h>
+#include <projeto_semear/kine_control.h>
+#include <cstdint>
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "depositar_client");
+    ros::NodeHandle node;
+
+    // Cria o serviço
+    ros::ServiceClient depositar_client = node.serviceClient<projeto_semear::DepositarContainer>("depositar_container");
+
+    projeto_semear::DepositarContainer srv;
+
+    srv.request.code = 1;
+    depositar_client.call(srv);
+    return 0;
+}
