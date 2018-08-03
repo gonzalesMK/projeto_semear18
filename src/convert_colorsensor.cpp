26c27e22104a9724fcc2d16eb387b22c4267551f
gonzalesmk <judcnegri@gmail.com>
2018-08-02 22:06:38 -0300

    Aumentado Convert ColorSensor para incluir as garras e Launch file aumentada com o mapa

--- src/convert_colorsensor.cpp
+++ src/convert_colorsensor.cpp
@@ -21,11 +21,15 @@ ros::Publisher pubBR;
 ros::Publisher pubFR;
 ros::Publisher pubBL;
 ros::Publisher pubFL;
+ros::Publisher pubGarraR;
+ros::Publisher pubGarraL;
 
 void callbackBR(const sensor_msgs::ImageConstPtr &msg);
 void callbackBL(const sensor_msgs::ImageConstPtr &msg);
 void callbackFR(const sensor_msgs::ImageConstPtr &msg);
 void callbackFL(const sensor_msgs::ImageConstPtr &msg);
+void callbackGarraR(const sensor_msgs::ImageConstPtr &msg);
+void callbackGarraL(const sensor_msgs::ImageConstPtr &msg);
 
 int main(int argc, char **argv)
 {
@@ -37,11 +41,19 @@ int main(int argc, char **argv)
     ros::Subscriber subBR = n.subscribe("/AMR/lineSensorBR", 1000, callbackBR);
     ros::Subscriber subFR = n.subscribe("/AMR/lineSensorFR", 1000, callbackFR);
     ros::Subscriber subFL = n.subscribe("/AMR/lineSensorFL", 1000, callbackFL);
+    ros::Subscriber subGarraR = n.subscribe("/AMR/ColorSensorGarraL", 1000, callbackGarraR);
+    ros::Subscriber subGarraL = n.subscribe("/AMR/ColorSensorGarraR", 1000, callbackGarraL);
 
+    
+    
+
+    pubBL = n.advertise<std_msgs::Float32>("/image_converter/lineSensorBL", 1);
     pubBR = n.advertise<std_msgs::Float32>("/image_converter/lineSensorBR", 1);
     pubFR = n.advertise<std_msgs::Float32>("/image_converter/lineSensorFR", 1);
-    pubBL = n.advertise<std_msgs::Float32>("/image_converter/lineSensorBL", 1);
     pubFL = n.advertise<std_msgs::Float32>("/image_converter/lineSensorFL", 1);
+    pubGarraR = n.advertise<std_msgs::Float32>("/image_converter/sensorGarraR", 1);
+    pubGarraL = n.advertise<std_msgs::Float32>("/image_converter/sensorGarraL", 1);
+    
 
     ros::spin();
 
@@ -75,4 +87,19 @@ void callbackFL(const sensor_msgs::ImageConstPtr &msg)
     message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
 
     pubFL.publish(message);
+}
+
+void callbackGarraR(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubGarraR.publish(message);
+}
+void callbackGarraL(const sensor_msgs::ImageConstPtr &msg)
+{
+    std_msgs::Float32 message;
+    message.data = sqrt(pow(msg->data[0], 2) + pow(msg->data[1], 2) + pow(msg->data[2], 2));
+
+    pubGarraL.publish(message);
 }
