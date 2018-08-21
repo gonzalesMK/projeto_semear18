bcd62b91180d248e4530f11a73719d806af055d7
thaleslk <thaleslkobosighawa@gmail.com>
2018-08-21 18:14:05 -0300

    arrumando para server e modificando depositar

--- src/depositar_server.cpp
+++ src/depositar_server.cpp
@@ -4,8 +4,11 @@
 #include <projeto_semear/moveEletroimaAction.h>
 #include <std_msgs/Bool.h>
 #include <projeto_semear/kine_control.h>
+#include <projeto_semear/GetContainerInfo.h>
+#include <vector>
+#include <projeto_semear/GetPose.h>
 
-int code = 1;
+int code = 0;
 
 /* Código para depositar o container na doca correta.
   Para execução do código, considera-se que o robô já está alinhado à doca certa e que 
@@ -35,11 +38,11 @@ void activeCb()
 bool depositar_container(projeto_semear::DepositarContainer::Request &req,
          projeto_semear::DepositarContainer::Response &res)
 {
-
-  ros::NodeHandle nh;
+  ros::NodeHandle node;
 
   kineControl::robot motor;
-  ros::Publisher pub = nh.advertise<std_msgs::Bool>("/AMR/activateEletroima", 1);
+
+  ros::Publisher pub = node.advertise<std_msgs::Bool>("/AMR/activateEletroima", 1);
   ros::Duration(0.5).sleep();
   ros::spinOnce();
   
@@ -52,35 +55,73 @@ bool depositar_container(projeto_semear::DepositarContainer::Request &req,
   client.waitForServer();
 
   projeto_semear::moveEletroimaGoal goal;
-  
+
+  //utilizando o servico de gps para encontrar a localização
+  ros::ServiceClient pose_client = node.serviceClient<projeto_semear::GetPose>("gps");
+  projeto_semear::GetPose srv;
+
+  //pegando a localização
+  pose_client.call(srv);
+  int localizacao_aux = (std::uint8_t)srv.response.pose.location;
+
+  //transformando a localização_aux para localização correta
+  int localizacao;
+  switch(localizacao_aux){
+    case 3:
+      localizacao = 12;
+
+    case 4:
+      localizacao = 13;
+  }
+
+  //utilizando o servico de mapa_container
+  ros::ServiceClient get_client = node.serviceClient<projeto_semear::GetContainerInfo>("getContainerInfo");
+  projeto_semear::GetContainerInfo get_srv;
+
+  srv.request.set = false;
+
+  //informando a localizacao da pilha onde o robo se encontra
+  get_srv.request.where = localizacao;
+  get_client.call(get_srv);
+
+  //pegando o vetor que contem os containers depositados
+  std::vector<std::uint8_t> vec = get_srv.response.lista;
+
+  int code = vec.size(); //variável que guarda quantos containers têm em uma pilha
+
+  ROS_INFO_STREAM("Valor do code:" << code);
+
   /*Code == 0: nenhum container depositado
     Code != 0: já existe um ou mais containers na pilha*/
 
-  if(code == 0){
-      goal.deslocamento.angular.z = 0;
-      goal.deslocamento.linear.x = 0;
-      goal.deslocamento.linear.y = 0;
-      goal.deslocamento.linear.z = -0.137;
-      client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
-      client.waitForResult(ros::Duration());
-      code++;
-  }else{
-      //alinhar com o container de baixo
-      kineControl::alinhar_containerdepositado(motor);
-      goal.deslocamento.angular.z = 0;
-      goal.deslocamento.linear.x = 0;
-      goal.deslocamento.linear.y = 0;
-      goal.deslocamento.linear.z = -0.137+(code*0.02); //0,2 chute da altura do container
-      client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
-      client.waitForResult(ros::Duration());
-      code++;
-  }
+  //alinhar com o container de baixo
+  if(code != 0) kineControl::alinhar_containerdepositado(motor);
+  
+  goal.deslocamento.angular.z = 0;
+  goal.deslocamento.linear.x = 0;
+  goal.deslocamento.linear.y = 0;
+  goal.deslocamento.linear.z = -0.137+(code*0.02); //0,2 chute da altura do container
+  client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+  client.waitForResult(ros::Duration());
+  
+  //andar uma distância determinada para fica no meio do container já depositado
+  //goal.deslocamento.angular.z = 1;
+  //goal.deslocamento.linear.z = 0;
+  //client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+  //client.waitForResult(ros::Duration());
 
   ROS_INFO_STREAM("desligando o eletroima");
   msg.data = false;
   pub.publish(msg);
-
   client.waitForResult(ros::Duration());
+
+  goal.deslocamento.angular.z = 0;
+  goal.deslocamento.linear.x = 0;
+  goal.deslocamento.linear.y = 0;
+  goal.deslocamento.linear.z = +0.137-(code*0.02); //0,2 chute da altura do container
+  client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+  client.waitForResult(ros::Duration());
+
   return 0;
 }
 
