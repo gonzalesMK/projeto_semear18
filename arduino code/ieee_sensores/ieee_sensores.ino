d036fcf78d6765933711ee4c1065a3d605947e74
gonzalesmk <judcnegri@gmail.com>
2018-10-31 11:50:21 -0300

    arduino

--- arduino code/ieee_sensores/ieee_sensores.ino
+++ arduino code/ieee_sensores/ieee_sensores.ino
@@ -1,5 +1,4 @@
 #include <ros.h>
-#include <ros/time.h>
 #include <projeto_semear/ArduinoRGB.h>
 #include <projeto_semear/Infra_Placa_Sensores.h>
 #include <projeto_semear/Enable_Placa_Sensores.h>
@@ -8,28 +7,29 @@
 #include "Adafruit_TCS34725.h"
 
 /* Pinouts Infravermelhos */
-#define infra_FR A0
-#define infra_FL A1
+#define infra_FR A6
+#define infra_FL A0
 #define infra_BR A2
 #define infra_BL A3
-#define infra_SR A6
+#define infra_SR A1
 #define infra_SL A7
 
-#define SDApin A4
-#define SCLpin A5
+//#define SDApin A4
+//#define SCLpin A5
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 projeto_semear::ArduinoRGB rgb;
 projeto_semear::Infra_Placa_Sensores infras;
 
 ros::NodeHandle nh;
+
 ros::Publisher pub_rgb("/AMR/arduinoSensoresRGB", &rgb ); //  RGB info
 ros::Publisher pub_infras("/AMR/arduinoSensoresInfras", &infras ); //  Infra Info
 
 void enable_callback(const projeto_semear::Enable_Placa_Sensores &msg ); // Enable Callback
 
-ros::Subscriber<projeto_semear::Enable_Placa_Sensores> sub_enables("/AMR/enableElevadores", &enable_callback); // Subscrive to enable topic
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+ros::Subscriber<projeto_semear::Enable_Placa_Sensores> sub_enables("/AMR/enableRGBSensores", &enable_callback); // Subscrive to enable topic
 
 bool enable_rgb = false;
 
@@ -37,30 +37,64 @@ void setup() {
 
   nh.getHardware()->setBaud(57600);
   nh.initNode();
-  
+
   nh.advertise(pub_infras);
   nh.advertise(pub_rgb);
   nh.subscribe(sub_enables);
-  
-  pinMode(infra_FR, INPUT);
-  pinMode(infra_FL, INPUT);
-  pinMode(infra_BR, INPUT);
-  pinMode(infra_BL, INPUT);
-  pinMode(infra_SR, INPUT);
-  pinMode(infra_SL, INPUT);
+
+  pinMode(infra_FR, INPUT_PULLUP);
+  pinMode(infra_FL, INPUT_PULLUP);
+  /* pinMode(infra_BR, INPUT);
+    pinMode(infra_BL, INPUT);
+    pinMode(infra_SR, INPUT);
+    pinMode(infra_SL, INPUT);*/
+
+  /* Set the RGB */
+  if (!tcs.begin())  while (10);
+
+
 }
 
 void loop() {
 
+  if ( !enable_rgb) {
     infras.infraFR = analogRead(infra_FR);
     infras.infraFL = analogRead(infra_FL);
-    infras.infraBR = analogRead(infra_BR);
-    infras.infraBL = analogRead(infra_BL);
-    infras.infraSR = analogRead(infra_SR);
-    infras.infraSL = analogRead(infra_SL);
-
+    infras.infraBR = 0;
+    infras.infraBL = 0;
+    infras.infraSR = 0;
+    infras.infraSL = 0;
+    /*    infras.infraBR = analogRead(infra_BR);
+        infras.infraBL = analogRead(infra_BL);
+        infras.infraSR = analogRead(infra_SR);
+        infras.infraSL = analogRead(infra_SL);
+    */
     pub_infras.publish(&infras);
-    delay(10);
+
+    rgb.red = 0;
+    rgb.green = 0;
+    rgb.blue = 0;
+
+    pub_rgb.publish(&rgb);
+
+  }
+  
+  if ( enable_rgb ) {
+    uint16_t clear, red, green, blue;
+
+    tcs.getRawData(&red, &green, &blue, &clear);
+  
+    rgb.red = red;
+    rgb.green = green;
+    rgb.blue = blue;
+
+    pub_rgb.publish(&rgb);
+
+  }
+
+  nh.spinOnce();
+
+  delay(25);
 }
 
 void enable_callback( const projeto_semear::Enable_Placa_Sensores &msg )
