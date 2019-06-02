449b958c386ff99436637a117808c107513f0223
gonzalesmk <gonzalesmk>
2019-06-02 17:04:12 -0500

    Refactoring #29

--- scripts/goToFirstPose.py
+++ scripts/goToFirstPose.py
@@ -5,6 +5,7 @@ import actionlib
 from projeto_semear.msg import goToFirstPoseAction, goToFirstPoseFeedback, goToFirstPoseResult
 from std_msgs.msg import Float64, Bool
 from lineSensors import LineSensor, Sides
+from motorControlLib import MotorControl, Wheels
 
 class goToFirstPoseServer(object):
     # Messages to Publish Feedback/result
@@ -17,61 +18,50 @@ class goToFirstPoseServer(object):
         self._as.start()
 
     def execute_cb(self, goal):
-        delay = rospy.Rate(10)
-        # Go Straight ahead until find line 
-        pubFL = rospy.Publisher('/motorFL/desired_vel', Float64, queue_size=10)    
-        pubBL = rospy.Publisher('/motorBL/desired_vel', Float64, queue_size=10)    
-        pubFR = rospy.Publisher('/motorFR/desired_vel', Float64, queue_size=10)    
-        pubBR = rospy.Publisher('/motorBR/desired_vel', Float64, queue_size=10)
-        pubEnable = rospy.Publisher('/encoder_enable', Bool, queue_size=10)    
         
         linesensors = LineSensor()
-        delay.sleep()
-        pubEnable.publish(True)
+        motorControl = MotorControl()
         
-        pubFL.publish(1)
-        pubBL.publish(1)
-        pubFR.publish(1)
-        pubBR.publish(1)
+        motorControl.setVelocity([1,1,1,1])
 
+        # 1) Get through the green line
+        rospy.loginfo("1")
         r = rospy.Rate(100)
-        # Get through the green line
         while(not rospy.is_shutdown()):
             rospy.loginfo( linesensors.readLines() )            
             sensors = linesensors.readLines()
             
             if( sensors[ int( Sides.LEFT) ] == 2 and sensors[ int(Sides.RIGHT) ] == 2 ):
                 rospy.loginfo( "Passou da linha" )            
-                pubEnable.publish(False)
                 linesensors.reset()
                 break
                        
             r.sleep()
-
-        pubTarget = rospy.Publisher('/desired_pose', Float64, queue_size=10)    
-        pubFL = rospy.Publisher('/motorSensorFL/error', Float64, queue_size=10)    
-        pubBL = rospy.Publisher('/motorSensorBL/error', Float64, queue_size=10)    
-        pubFR = rospy.Publisher('/motorSensorFR/error', Float64, queue_size=10)    
-        pubBR = rospy.Publisher('/motorSensorBR/error', Float64, queue_size=10)
-        pubEnable2 = rospy.Publisher('/pid_enable', Bool, queue_size=10)    
         
-        delay.sleep()
-        delay.sleep()
-        delay.sleep()
-
-        pubEnable2.publish(True)
-        pubTarget.publish(0.0)
+        error = [0,0,0,0]
+        sensors = linesensors.readLines()
         
-        # Align with the Next Line (Black)
+        error[int(Wheels.FL)] = sensors[int(Sides.LEFT)]
+        error[int(Wheels.FR)] = sensors[int(Sides.RIGHT)]
+        error[int(Wheels.BL)] = sensors[int(Sides.LEFT)]
+        error[int(Wheels.BR)] = sensors[int(Sides.RIGHT)] 
+
+        motorControl.align(error)        
+        
+        # 2) Align with the Next Line (Black)
+        rospy.loginfo("2")
         is_stable = 0
         while(not rospy.is_shutdown()):
             rospy.loginfo( linesensors.readLines() )            
+
             sensors = linesensors.readLines()
             
-            pubFL.publish(sensors[int(Sides.LEFT)]*10.)
-            pubBL.publish(sensors[int(Sides.LEFT)]*10.)
-            pubFR.publish(sensors[int(Sides.RIGHT)]*10.)
-            pubBR.publish(sensors[int(Sides.RIGHT)]*10.)
+            error[int(Wheels.FL)] = sensors[int(Sides.LEFT)]
+            error[int(Wheels.FR)] = sensors[int(Sides.RIGHT)]
+            error[int(Wheels.BL)] = sensors[int(Sides.LEFT)]
+            error[int(Wheels.BR)] = sensors[int(Sides.RIGHT)] 
+
+            motorControl.align(error)        
             
             if( sensors[ int(Sides.LEFT)] == 0 and sensors[int(Sides.RIGHT)] == 0 ):
                 is_stable += 1
@@ -82,8 +72,49 @@ class goToFirstPoseServer(object):
                        
             r.sleep()
         
-        self._as.set_succeeded()
+        # 3 ) Go to the right following the line
+        while(not rospy.is_shutdown()):
+            rospy.loginfo( linesensors.readLines() )            
 
+            sensors = linesensors.readLines()
+            
+            error[int(Wheels.FL)] = sensors[int(Sides.LEFT)]  + 2
+            error[int(Wheels.FR)] = sensors[int(Sides.RIGHT)] - 2
+            error[int(Wheels.BL)] = sensors[int(Sides.LEFT)]  - 2
+            error[int(Wheels.BR)] = sensors[int(Sides.RIGHT)] + 2
+
+            motorControl.align(error)        
+            
+            if( sensors[int(Sides.FRONT)] == 0 ):
+                break
+                       
+            r.sleep()
+        
+        # 4) Final Alignment
+        is_stable = 0
+        while(not rospy.is_shutdown()):
+            rospy.loginfo( linesensors.readLines() )            
+
+            sensors = linesensors.readLines()
+            
+            error[int(Wheels.FL)] = sensors[int(Sides.LEFT)]
+            error[int(Wheels.FR)] = sensors[int(Sides.RIGHT)]
+            error[int(Wheels.BL)] = sensors[int(Sides.LEFT)]
+            error[int(Wheels.BR)] = sensors[int(Sides.RIGHT)] 
+
+            motorControl.align(error)        
+            
+            if( sensors[ int(Sides.LEFT)] == 0 and sensors[int(Sides.RIGHT)] == 0 ):
+                is_stable += 1
+                if( is_stable == 100):
+                    break
+            else:
+                is_stable = 0
+                       
+            r.sleep()
+
+            motorControl.stop()
+            self._as.set_succeeded()
 
 if __name__ == '__main__':
     rospy.init_node('goToFirstPose')
