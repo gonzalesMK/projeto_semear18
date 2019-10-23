d31d7ada60d21f9c0a3aa23771bb300c2f81b3d9
gonzalesmk <judcnegri@gmail.com>
2019-10-23 16:04:48 -0300

    before arduino convertion

--- robot_hardware/src/sensorCode.py
+++ robot_hardware/src/sensorCode.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import rospy
 import serial
+import struct
 import numpy as np
 from std_msgs.msg import Int64,UInt8
+
+
 
 if __name__ == "__main__":
     
@@ -20,26 +23,32 @@ if __name__ == "__main__":
     pubLimitSwitchs = rospy.Publisher("/claw/limitSwitchs",UInt8 , queue_size=1)
     pubEncoder = rospy.Publisher("/claw/height",Int64 , queue_size=1)
     pubContainers = rospy.Publisher("/containerSensor",UInt8 , queue_size=1)
-
+    b = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     while( not rospy.is_shutdown() ):
-        try :
-            with serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1) as ser:
+            with serial.Serial('/dev/ttyUSB0', 115200, timeout=.01) as ser:
                 while( not rospy.is_shutdown() ):
-                    b = np.array( ser.read(13), dtype= np.int64)
-
-                    pFL.publish(b[0])
-                    pFR.publish(b[1])
-                    pBL.publish(b[2])
-                    pBR.publish(b[3])
-                    pLF.publish(b[4])
-                    pLB.publish(b[5])
-                    pRF.publish(b[6])
-                    pRB.publish(b[7])
-
-                    pubContainers.publish(b[8])
-                    pubLimitSwitchs.publish(b[9])
+                    a = struct.pack('!B', 68)
+                    ser.write(a)
+                    a = str(ser.read(14))
                     
-                    pubEncoder.publish( -( b[10] + b[11] * 256 + b[12] * 65536 + b[13] * 16777216 - 2147483648) )
-        except:
-            print("Trying again soon")
-            rospy.Rate(1).sleep()    
+                    
+                    if len(a) == 14:
+                        j = 0
+                        for i in a:
+                            try:
+                                b[j] = ord( i)
+                                #int_values = struct.unpack('>13B', a)        
+                                j = j + 1
+                            except :
+                                print("exception")
+                                pass
+                        
+                        print("j: {}".format(j))
+                        
+                        if j :
+                            b = np.array( b, dtype= np.int64)
+                            print(b)
+                            print( "A0: {} A1: {} A2: {} A3: {} A4: {} A5: {} A6: {} A7: {}".format( b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]))
+                            print("Containers: {}".format(b[8]))
+                            print("Limit: {}".format([9]))
+                            print("encoder: {}".format( -( b[10] + b[11] * 256 + b[12] * 65536 + b[13] * 16777216 - 2147483648) ))
